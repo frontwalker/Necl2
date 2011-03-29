aff4f7d454cb14220b664cdf86f1a9b81cd2102e
Jonatan Hagglund <jonhag@.itm.miun.se>
2011-03-29 12:41:12 +0200

    Created logic for about page, untranslated

---
+++ src/Logistikcenter.Web/Controllers/AboutController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Logistikcenter.Web.Controllers
+{
+    public class AboutController : Controller
+    {
+        //
+        // GET: /About/
+
+        public ActionResult Index()
+        {
+            ViewBag.title = "Om Logistikcenter";
+
+            return View();
+        }
+
+    }
+}
